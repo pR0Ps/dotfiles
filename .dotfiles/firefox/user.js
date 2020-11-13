569cd15ad7fdb4fc0219cdbc6841d2530cdcf486
Carey Metcalfe <carey@cmetcalfe.ca>
2020-11-13 11:19:30 -0500

    Firefox: reorganize settings and add some comments

--- .dotfiles/firefox/user.js
+++ .dotfiles/firefox/user.js
@@ -2,18 +2,20 @@
 //
 // If you aren't me then read up on the option before blindly applying.
 // There are some tradeoffs in here you might not agree with.
+//
+// Information on preferences can be found in the source code. See https://searchfox.org/
 
 
 // Don't warn when modifying about:config
 user_pref("browser.aboutConfig.showWarning", false);
 
 
-// ----- Preferences/Interface tweaks -----
+// ----- Interface tweaks -----
 // Compact UI (smaller back button, etc)
 user_pref("browser.uidensity", 1);
 
 // Use the platform's native title bar
-// (This complements other tweaks that move the tabs from the title bar into a sidebar)
+// (This complements the addon + CSS tweaks that moves the tabs from the title bar into a sidebar)
 user_pref("browser.tabs.drawInTitlebar", false);
 
 // Display the URL exactly as it was entered (don't trim slashes, remove http, etc)
@@ -26,26 +28,15 @@ user_pref("network.IDN_show_punycode", true);
 user_pref("browser.urlbar.matchBuckets", "general:5,suggestion:Infinity");
 
 // Don't suggest open tabs when typing in the URL bar
+// (replaced with the TabSearch addon)
 user_pref("browser.urlbar.suggest.openpage", false);
 
 // Don't suggest popular websites just because they're popular
 user_pref("browser.urlbar.usepreloadedtopurls.enabled", false);
 
-// Disable pasting and loading URLs with middle mouse button
-user_pref("middlemouse.contentLoadURL", false);
-
-// Enable spell check in all text boxes (default is just multiline)
-user_pref("layout.spellcheckDefault", 2);
-
-// Don't automatically copy selected text to clipboard (Linux-only feature)
-user_pref("clipboard.autocopy", false);
-
 // Force links to open in new tabs instead of new windows
 user_pref("browser.link.open_newwindow", 3);
 user_pref("browser.link.open_newwindow.restriction", 0);
-
-// Disable Pocket
-user_pref("extensions.pocket.enabled", false);
 
 // Write session restore data every 30 seconds (default is 15)
 user_pref("browser.sessionstore.interval", 30000);
@@ -69,12 +60,23 @@ user_pref("browser.download.hide_plugins_without_extensions", false);
 user_pref("browser.startup.page", 3);
 user_pref("browser.startup.homepage", "about:blank");
 
-// Don't check if FF is the default browser
-user_pref("browser.shell.checkDefaultBrowser", false);
-
 // Disable new tab page
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.newtab.url", "about:blank");
+
+
+// ----- Preference tweaks -----
+// Don't check if FF is the default browser
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+// Disable pasting and loading URLs with middle mouse button
+user_pref("middlemouse.contentLoadURL", false);
+
+// Don't automatically copy selected text to clipboard (Linux-only feature)
+user_pref("clipboard.autocopy", false);
+
+// Enable spell check in all text boxes (default is just multiline)
+user_pref("layout.spellcheckDefault", 2);
 
 // Increase the amount of history that is retained
 user_pref("places.history.expiration.max_pages", 10000000);
@@ -85,11 +87,14 @@ user_pref("places.history.expiration.max_pages", 10000000);
 user_pref("dom.ipc.plugins.reportCrashURL", false);
 
 // Disable recommendations
-user_pref("extensions.getAddons.showPane", false); // Hide "Recommended" tab in about:addons
+user_pref("extensions.getAddons.showPane", false); // Hides the "Recommended" tab in about:addons
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.discovery.enabled", false);
 
 // Disable studies and experiments
+// Telemetry is left enabled since I want my usage to be represented in their
+// collected data. However, I'm not comfortable with my browser running random
+// experiments, installing addons, or changing its settings.
 user_pref("app.normandy.enabled", false);
 user_pref("messaging-system.rsexperimentloader.enabled", false);
 
@@ -102,8 +107,7 @@ user_pref("network.captive-portal-service.enabled", false);
 
 // Disable location bar domain guessing (ie. trying x.com if x doesn't resolve)
 user_pref("browser.fixup.alternate.enabled", false);
-// Don't send user:pass to guessed domains if guessing is enabled
-user_pref("browser.fixup.hide_user_pass", true);
+user_pref("browser.fixup.hide_user_pass", true); // Don't send user:pass to guessed domains if guessing is enabled
 
 // Don't submit content typed in the URL/search bar to search engines automatically
 // (disables search suggestions)
@@ -122,7 +126,7 @@ user_pref("network.prefetch-next", false);
 // Don't send referer header cross-domain (0=always send, 1=base domain match, 2=full domain match)
 user_pref("network.http.referer.XOriginPolicy", 1);
 
-// Display true origin of premission prompts (don't allow sites to delegate to iframe/etc)
+// Display true origin of permission prompts (don't allow sites to delegate to iframe/other)
 user_pref("permissions.delegation.enabled", false);
 
 
@@ -149,7 +153,10 @@ user_pref("dom.targetBlankNoOpener.enabled", true);
 user_pref("network.stricttransportsecurity.preloadlist", false);
 
 
-// ----- Disable unwanted APIs -----
+// ----- Disable unwanted APIs/features -----
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
 // Prevent sites from messing with window chrome, size, and position
 user_pref("dom.disable_window_open_feature.close", true);
 user_pref("dom.disable_window_open_feature.location", true);
@@ -197,5 +204,4 @@ user_pref("network.websocket.max-connections", 0);
 // Disable sending beacons and pings
 user_pref("beacon.enabled", false);
 user_pref("browser.send_pings", false);
-// Require pings to at least be the same domain if enabled
-user_pref("browser.send_pings.require_same_host", true);
+user_pref("browser.send_pings.require_same_host", true);  // Require pings to at least be the same domain if enabled
